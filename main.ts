c65183ee7324d42d8704d42f04b69c2c22bee9a3
Gustav X <sillygorilla42@gmail.com>
2025-11-05 17:29:05 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+scene.setBackgroundColor(9)
+tiles.setCurrentTilemap(tilemap`level2`)
+let mySprite = sprites.create(img`
+    . . . . . . . f f f f f . . . . 
+    . . . . . . f e e e e e f . . . 
+    . . . . f f e e e d d d d f . . 
+    . . . f d d e e d d d d d d c . 
+    . . . c d b e e d f d d f d c . 
+    f f . c d b e e d f d d f d d c 
+    f e f . c f e e d d d d e e d c 
+    f e f . . f e e e d c d d d d c 
+    f e f . . f f e e e d c c c f . 
+    f e f . f e e e e f f f f f . . 
+    . f f f e e e e e e e f . . . . 
+    . . f e e e e f e e f e f f . . 
+    . . f e e e f f f e e f f e f . 
+    . f b f f f f f f c d d b d d f 
+    . f d d c f . . f d d d c d d f 
+    . . f f f . . . f f f f f f f . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
